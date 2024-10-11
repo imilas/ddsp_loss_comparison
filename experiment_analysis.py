36559321e3dafabf6857bfd1d42e72203392cace
imilas <asalimi@ualberta.ca>
2024-10-11 15:52:02 -0600

    update

--- experiment_analysis.py
+++ experiment_analysis.py
@@ -8,16 +8,34 @@ app = marimo.App()
 def __():
     import marimo as mo
     from helpers.experiment_scripts import load_json
-    return load_json, mo
+    import numpy as np
+    return load_json, mo, np
 
 
-app._unparsable_cell(
-    r"""
-    d = load_json(\"results/experiments.json\")
-    d = 
-    """,
-    name="__"
-)
+@app.cell
+def __(load_json):
+    d = load_json("results/experiments.json")
+    d[0]
+    return d,
+
+
+@app.cell
+def __():
+    lfn_names = ['L1_Spec' , 'DTW_Onset', 'SIMSE_Spec', 'JTFS']
+    return lfn_names,
+
+
+@app.cell
+def __(d, lfn_names, np):
+    def get_p_error(e):
+        p1 = np.array(list(e["true_params"]["params"].values()))
+        p2 = np.array(list(e["norm_params"].values()))[:,-1]
+        return np.sqrt(np.sum((p1-p2)**2))
+    def filter_experiments(d,loss_fn_name,prog_num):
+        return [x for x in d if x["loss"]==loss_fn_name and x["program_id"]==prog_num]
+
+    [[get_p_error(x) for x in filter_experiments(d,lfn_name,1)] for lfn_name in lfn_names]
+    return filter_experiments, get_p_error
 
 
 if __name__ == "__main__":
